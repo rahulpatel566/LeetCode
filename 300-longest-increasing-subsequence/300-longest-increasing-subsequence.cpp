a3e86844202b116bcc59b3508499144e78e8b117
rahulpatel566 <66542244+rahulpatel566@users.noreply.github.com>
2022-06-14 18:26:12 +0530

    Time: 327 ms (52.12%), Space: 10.6 MB (20.48%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,15 +1,33 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> v;
-        for(auto &x:nums)
+        
+        //lis using binary search
+        // vector<int> v;
+        // for(auto &x:nums)
+        // {
+        //     auto it=lower_bound(v.begin(),v.end(),x);
+        //     if(it!=v.end())
+        //        *it=x;
+        //     else
+        //         v.push_back(x);
+        // }
+        // return (int)v.size();
+        
+        //using tabulation dp n^2
+        int ans=1,n=nums.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++)
         {
-            auto it=lower_bound(v.begin(),v.end(),x);
-            if(it!=v.end())
-               *it=x;
-            else
-                v.push_back(x);
+            for(int j=0;j<i;j++)
+            {
+                if(nums[i]>nums[j])
+                {
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+           ans=max(ans,dp[i]);
         }
-        return (int)v.size();
+        return ans;
     }
 };
