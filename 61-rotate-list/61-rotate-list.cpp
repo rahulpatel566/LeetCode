6429416bbf108c94862e1a05894232d15d465f1b
rahulpatel566 <66542244+rahulpatel566@users.noreply.github.com>
2022-06-14 07:15:05 +0530

    Time: 7 ms (85.54%), Space: 11.8 MB (55.56%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -14,25 +14,48 @@ public:
         if(!head)
             return head;
         ListNode* temp=head;
-        vector<int> arr;
+        // vector<int> arr;
+        // while(temp)
+        // {
+        //     arr.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int n=arr.size();
+        // k=k%n;
+        // reverse(arr.begin(),arr.end());
+        // //reverse first k element
+        // reverse(arr.begin(),arr.begin()+k);
+        // //reverse last k element
+        // reverse(arr.begin()+k,arr.end());
+        // temp=head;
+        // for(auto &x:arr)
+        // {
+        //     temp->val=x;
+        //     temp=temp->next;
+        // }
+        // return head;
+        
+        int n=0;
+        ListNode* tail;
         while(temp)
         {
-            arr.push_back(temp->val);
+            n++;
+            tail=temp;
             temp=temp->next;
         }
-        int n=arr.size();
         k=k%n;
-        reverse(arr.begin(),arr.end());
-        //reverse first k element
-        reverse(arr.begin(),arr.begin()+k);
-        //reverse last k element
-        reverse(arr.begin()+k,arr.end());
+        if(k==0)
+            return head;
+        k=n-k; //node will be head
         temp=head;
-        for(auto &x:arr)
+        while(k>1)
         {
-            temp->val=x;
+            k--;
             temp=temp->next;
         }
-        return head;
+        ListNode* Head=temp->next;
+        temp->next=NULL;
+        tail->next=head;
+        return Head;
     }
 };
